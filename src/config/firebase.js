613ba2e8877a084723debc0597d9916e9e985995
Sidsmartz <siddhartht4206@gmail.com>
2025-04-03 10:07:20 +0530

    env

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,14 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAO4mvdAJwxvcxvQp3WV6kuppuboNYcVVE",
-  authDomain: "b2bstall.firebaseapp.com",
-  databaseURL: "https://b2bstall-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "b2bstall",
-  storageBucket: "b2bstall.firebasestorage.app",
-  messagingSenderId: "71474750145",
-  appId: "1:71474750145:web:7fce99dec054440accea40",
-  measurementId: "G-FTZM3V3YXV"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
