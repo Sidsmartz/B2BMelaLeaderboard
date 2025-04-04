70b3db3039835c3442491dc5db2e1bdd5f94ea60
Siddharth T S <119348927+Sidsmartz@users.noreply.github.com>
2025-04-04 09:40:43 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
     <div className="container">
       <h1>Stall Management System</h1>
       <p>Welcome! Please <Link to="/login">Login</Link> to continue or if you are an admin, go to the <Link to="/admin">Admin Panel</Link>.</p>
-      <p>Welcome! Click <Link to="/leaderboard">here</Link> to go to the leaderboard</p>
+      <p>Welcome! Click <Link to="/styled-leaderboard">here</Link> to go to the leaderboard</p>
     </div>
   );
 };
